94c7cb5af4b08ab201c9152d6e273161de366a5c
Tancredi Trugenberger <tancredi.trugenberger@gmail.com>
2014-06-20 17:40:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+## API Service
+
+> Simple http service / api promised wrapper for client-side JavaScript or Node.js. This class helps you creating collections of [api-resources](https://github.com/KanoComputing/js-api-resource) with an intuitive and simple api.
+
+## Simple usage
+
+```javascript
+var Service = require('api-service');
+
+var myApiWrapper = new Service('http://localhost/api')
+
+    // Add users endpoints
+    .add('users.list', { method: 'get', route: 'users' })
+    .add('users.signup', { method: 'post', route: 'users/signup' })
+    .add('users.get.byId', { method: 'get', route: 'users/:id' })
+    .add('users.get.byUsername', { method: 'get', route: 'users/username/:username' })
+
+    // Add posts endpoints
+    .add('posts.save', { method: 'post', route: 'posts' })
+    .add('posts.list', { method: 'get', route: 'posts' })
+    .add('posts.get.byId', { method: 'get', route: 'posts/:id' })
+    .add('posts.delete', { method: 'delete', route: 'posts/:id' })
+
+    // Add a middleware function that adds authorization headers to request
+    .use(function (req, next) {
+        if (token) {
+            req.setRequestHeader('Authorization', token);
+        }
+
+        next();
+    });
+
+// GET /users
+myApiWrapper.users.list()
+.then(function (res) {
+    var users = res.body;
+}, function (res) {
+   var error = res.body;
+});
+
+// GET /users?q=foo
+myApiWrapper.users.list({}, { q: 'foo' })
+.then(function (res) {
+    var users = res.body;
+});
+
+
+// POST /posts with payload
+myApiWrapper.posts.save({ title: 'Foo bar', text: 'Some text...' })
+.then(function (res) {
+    var success = res.body.success;
+}, function (res) {
+   var error = res.body;
+});
+
+// DELETE /posts/some_id
+myApiWrapper.posts.save({ id: 'some_id' })
+.then(function (res) {
+    var success = res.body.success;
+}, function (res) {
+   var error = res.body;
+});
+
+## Service
+
+**This class is used to group and organise instances of the [api-resource](https://github.com/KanoComputing/js-api-resource) class.**
+Service instances are also event-emitters and they will propagate all events sent by any of their `Resource` children.
+
+### Constructor
+
+* `baseUrl` (*String*) Service base URL (E.g. `'http://www.my-api.com'`) - without `/` at the end
+
+#### Events
+
+The events propagated by this class are documented in the [Resource](https://github.com/KanoComputing/js-api-resource) readme.
+
+#### Methods
+
+`.add(namespace, [ options ])` Add an endpoint to the service. This will be an instance of [Resource](https://github.com/KanoComputing/js-api-resource) pass the options to its constructor
+`.use(middleware)` Add a middleware function to the resource. The function will be called on the `Resource` instance and receive as arguments the `XMLHttpRequest` request instance and the `next` function to proceed.
+
+## Test
+
+Tests are currently written on server-side only. Run `npm install` and `npm test` to test.
+
+## Contribution
+
+Contributions are welcome as long as documented and tested.
+
+## License
+
+Copyright (c) 2014 Kano Computing Ltd. - Released under the [MIT license](https://github.com/KanoComputing/js-api-resource/blob/master/LICENSE)
---
+++ test/Service.test.js
@@ -0,0 +1,138 @@
+var Service = require('../lib/Service'),
+    http = require('http'),
+    should = require('should'),
+    async = require('async');
+
+describe('Resouce - server', function() {
+    before(function (done) {
+        var server = http.createServer(function (req, res) {
+            var body = '';
+
+            req.on('data', function (data) {
+                body += data;
+
+                if (body.length > 1e6) { 
+                    req.connection.destroy();
+                }
+            });
+
+            req.on('end', function () {
+                res.writeHead(200);
+
+                res.write(JSON.stringify({
+                    payload: body ? JSON.parse(body) : null,
+                    method: req.method,
+                    url: req.url
+                }));
+
+                res.end();
+            });
+        });
+
+        server.listen(4567);
+        done();
+    });
+
+    it('instanciates without breaking', function () {
+        should(function () {
+            new Service();
+        }).not.throw();
+    });
+
+    it('correctly attaches methods to given namespaces', function () {
+        var service = new Service('http://localhost:4567')
+            .add('get.byName', { method: 'get' })
+            .add('get.foo.byId', { method: 'get' })
+            .add('get.foo.bar', { method: 'get' });
+
+        service.get.should.be.an.Object;
+        service.get.byName.should.be.a.Function;
+        service.get.foo.byId.should.be.a.Function;
+        service.get.foo.bar.should.be.a.Function;
+    });
+
+    it('resources send correct requests', function (done) {
+        var service = new Service('http://localhost:4567')
+            .add('list', { method: 'get' })
+            .add('post', { route: 'create/:username' })
+            .add('profile.update', { method: 'put', route: 'profile/update/:username' });
+
+        async.series([
+
+            function (callback) {
+                service.list({}, { foo: 'bar' })
+                .then(function (res) {
+                    res.should.eql({
+                        status: 200,
+                        body: {
+                            method: 'GET',
+                            payload: null,
+                            url: '/?foo=bar'
+                        }
+                    });
+
+                    callback();
+                });
+            },
+
+            function (callback) {
+                service.post({ username: 'john', foo: 'bar' })
+                .then(function (res) {
+
+                    res.should.eql({
+                        status: 200,
+                        body: {
+                            method: 'POST',
+                            payload: { username: 'john', foo: 'bar' },
+                            url: '/create/john'
+                        }
+                    });
+
+                    callback();
+                });
+            },
+
+            function (callback) {
+                service.profile.update({ username: 'marc', bar: 'foo' }, { q: '123' })
+                .then(function (res) {
+
+                    res.should.eql({
+                        status: 200,
+                        body: {
+                            method: 'PUT',
+                            payload: { username: 'marc', bar: 'foo' },
+                            url: '/profile/update/marc?q=123'
+                        }
+                    });
+
+                    callback();
+                });
+            }
+
+        ], done);
+    });
+
+    it('correctly bubbles up resources events', function (done) {
+        var service = new Service('http://localhost:4567')
+            .add('list', { method: 'get' });
+
+        service.once('response', function (res) {
+            res.status.should.equal(200);
+
+            service.once('success', function (res, resource) {
+                res.status.should.equal(200);
+
+                resource.method.should.equal('get');
+                resource.route.should.equal('http://localhost:4567');
+
+                done();
+            });
+
+            service.list();
+        });
+
+        service.list();
+
+    });
+
+});
